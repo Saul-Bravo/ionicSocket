07a0c5e14692f57563f11fe49599bd5a91c24161
Saul-Bravo <sauluriel1402@gmail.com>
2022-11-18 20:37:54 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,16 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+
+const config:SocketIoConfig = {
+  url: "http://localhost:3001",
+  options: {}
+}
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, SocketIoModule.forRoot(config)],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor() {}
+  messages:Array<any> = [{msg: "message", user: "saul"}, {msg: "message2", user:"otheruser"}];
+  currentUser:any;
+  textArea:any = "";
+
+  constructor(private socket:Socket, private toast:ToastController) {}
+
+  ngOnInit(): void {
+    this.socket.connect();
+    let name = `user-${new Date().getTime()}`;
+    this.currentUser = name;
+    this.socket.emit("set-name", name);
+    this.socket.fromEvent("user-change").subscribe((msg:any) => {
+      let user = msg["user"];
+      if (msg["event"] === "left") {
+        this.ShowToast("User "+ user +" left");
+      }
+      else {
+        this.ShowToast("User " + user + " joined")
+      }
+    });
+    this.socket.fromEvent("send-message").subscribe((msg:any) => {
+      this.messages.push({msg: msg["message"], user:this.currentUser});
+    });
+  }
+
+  async ShowToast(message:any) {
+    let toast = await this.toast.create({
+      message: message,
+      position: 'top',
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  SendMessage(message:any){
+    this.socket.emit("message", message);
+  }
 
 }
